fb6cb320edb3fbc2e83afbb7875b4edbdb5f2b6f
Karamysheva Irina <karamyshevaiv@mail.ru>
2021-06-22 17:51:15 +0500

    Refactor button type

--- app/src/main/java/ru/geekbrains/androidhomework2/MainActivity.java
+++ app/src/main/java/ru/geekbrains/androidhomework2/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cons_weights);
-        
+
         init();
 
 
