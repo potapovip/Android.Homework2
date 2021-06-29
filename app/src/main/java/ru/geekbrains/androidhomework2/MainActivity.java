7d525c2ffdab508b02ca1310787cf37c669a3389
Karamysheva Irina <karamyshevaiv@mail.ru>
2021-06-29 15:37:33 +0500

    Added radioButtons for changing themes.

--- app/src/main/java/ru/geekbrains/androidhomework2/MainActivity.java
+++ app/src/main/java/ru/geekbrains/androidhomework2/MainActivity.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 
@@ -26,8 +28,9 @@ public class MainActivity extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cons_weights);
-
+        setTheme(convertCodeToStyle(getAppTheme()));
         init();
+        initChanger();
         Intent intent = getIntent();
     String action = intent.getAction();
 //        String type = intent.getType();
@@ -115,5 +118,52 @@ public class MainActivity extends AppCompatActivity  {
         for (int i = 0; i < actionsIds.length; i++) {
             findViewById(actionsIds[i]).setOnClickListener(actionButtonClicListener);
         }
+    }
+    private final int MyCoolStyle = 0;
+    private final int NotMyNoralStyle = 1;
+    private final int WhateverStyle = 2;
+
+    private final String KEY_PREF = "key";
+    private final String APP_THEME = "theme";
+
+    private int convertCodeToStyle(int codeStyle) {
+        switch (codeStyle) {
+            case MyCoolStyle:
+                return R.style.MyCoolStyle;
+            case NotMyNoralStyle:
+                return R.style.NotMyNoralStyle;
+            default:
+                return R.style.WhateverStyle;
+        }
+    }
+
+    private void initChanger() {
+        initButton(findViewById(R.id.radioButton), MyCoolStyle);
+        initButton(findViewById(R.id.radioButton1), NotMyNoralStyle);
+        initButton(findViewById(R.id.radioButton2), WhateverStyle);
+    }
+
+    private void initButton(RadioButton button, int codeStyle) {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setAppTheme(codeStyle);
+                recreate();
+            }
+        });
+    }
+
+    private void setAppTheme(int codeStyle) {
+        SharedPreferences sharedPreferences = getSharedPreferences(KEY_PREF, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(APP_THEME, codeStyle);
+        editor.apply();
+//        codeStyle = 9999;
+//        Log.d("mylogs", sharedPreferences.getInt(APP_THEME, codeStyle)+"");
+    }
+
+    private int getAppTheme() {
+        SharedPreferences sharedPreferences = getSharedPreferences(KEY_PREF, MODE_PRIVATE);
+        return sharedPreferences.getInt(APP_THEME, MyCoolStyle);
     }
 }
--- app/src/main/java/ru/geekbrains/androidhomework2/Options.java
+++ app/src/main/java/ru/geekbrains/androidhomework2/Options.java
@@ -3,21 +3,67 @@ package ru.geekbrains.androidhomework2;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.RadioButton;
 
 public class Options extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(convertCodeToStyle(getAppTheme()));
         setContentView(R.layout.activity_options);
-        SwitchCompat switchCompat = findViewById(R.id.switchSize);
-        switchCompat.setOnClickListener(new View.OnClickListener() {
+        initChanger();
+    }
+
+    private final int MyCoolStyle = 0;
+    private final int NotMyNoralStyle = 1;
+    private final int WhateverStyle = 2;
+
+    private final String KEY_PREF = "key";
+    private final String APP_THEME = "theme";
+
+    private int convertCodeToStyle(int codeStyle) {
+        switch (codeStyle) {
+            case MyCoolStyle:
+                return R.style.MyCoolStyle;
+            case NotMyNoralStyle:
+                return R.style.NotMyNoralStyle;
+            default:
+                return R.style.WhateverStyle;
+        }
+    }
+
+    private void initChanger() {
+        initButton(findViewById(R.id.radioButton3), MyCoolStyle);
+        initButton(findViewById(R.id.radioButton4), NotMyNoralStyle);
+        initButton(findViewById(R.id.radioButton5), WhateverStyle);
+    }
+
+    private void initButton(RadioButton button, int codeStyle) {
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                setAppTheme(codeStyle);
                 recreate();
             }
         });
+    }
+
+    private void setAppTheme(int codeStyle) {
+        SharedPreferences sharedPreferences = getSharedPreferences(KEY_PREF, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(APP_THEME, codeStyle);
+        editor.apply();
+//        codeStyle = 9999;
+//        Log.d("mylogs", sharedPreferences.getInt(APP_THEME, codeStyle)+"");
+    }
+
+    private int getAppTheme() {
+        SharedPreferences sharedPreferences = getSharedPreferences(KEY_PREF, MODE_PRIVATE);
+        return sharedPreferences.getInt(APP_THEME, MyCoolStyle);
     }
 }
